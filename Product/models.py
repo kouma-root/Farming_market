4595d35b9478dc2262c70946b7d7eee9c7f0003e
kouma-root <parfaitkoumakang@gmail.com>
2025-08-13 01:52:03 +0100

    Initialiwing The Capstone project & creating Product app

---
+++ Product/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Product(models.Model):
+    States =[
+        ('Available', 'available'),
+        ('Sold Out', 'sold out'),
+    ]
+    name = models.CharField(max_length=128)
+    cathegory = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=0)
+    price = models.FloatField(default=0.0)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=25, choices=States)
+    thumbnail = models.ImageField(upload_to='products', blank=True, null=True)
