03491616cb6aa139c318023bdd51a52dbcce9420
kouma-root <parfaitkoumakang@gmail.com>
2025-08-17 10:05:09 +0100

    Adding template for the main(index) page

--- Product/models.py
+++ Product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Item(models.Model):
     States =[
@@ -16,3 +17,8 @@ class Item(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+    
+    def get_absolute_url(self):
+        
+        return reverse('product', kwargs={'slug':self.slug})
--- Product/views.py
+++ Product/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from Product.models import Item
+
 from django.http import HttpResponse
 
 def index(request):
     products = Item.objects.all()
-    
     return render(request, 'product/index.html', context={"products": products})
 
 
 def product_detail(request, slug):
-    return HttpResponse(slug)
+   product = get_object_or_404(Item, slug=slug)
+   return render(request, 'product/detail.html', context={'product': product})
+    
